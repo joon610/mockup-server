b2dd07e6556fa0b6756f275cf5ad7b90ba54aa74
joon <jdj610@gmail.com>
2019-12-03 23:33:21 +0900

    fix css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,19 @@
 <template lang="pug">
   v-container
-    v-layout(text-center='', wrap='')
       v-row.row-height(no-gutters)
-        .input-row
+        v-col(cols="8" sm="8")
           v-text-field(v-model="rootPath" :solo="true" :flat="true" readonly)
-        .select-root-container
+        v-col(cols="4" sm="4")
           v-btn(color="#A5D6A7" :disabled="serverStatus" @click="initPath()") Select Root
       v-row.row-height(no-gutters)
-        .input-row
+        v-col(cols="4" sm="4")
             .loacalhost-label http://localhost:
-            .loacalhost-input
+        v-col(cols="4" sm="4")
+            .loacalhost
               v-text-field(v-model="portNum" :solo="true" :readonly="isServerOn" :flat="true" style="hegiht:48px")
-        .server-btn
-            v-btn(v-show="isServerOn === false" color="primary"   :disabled="hasApiList" @click="startServer()") start Server
-            v-btn(v-show="isServerOn === true " color="deep-orange" :disabled="hasApiList" @click="closeServer()") close Server
+        v-col(cols="4" sm="4")
+            v-btn(v-if="isServerOn === false" color="primary"   :disabled="hasApiList" @click="startServer()") start Server
+            v-btn(v-else color="deep-orange" :disabled="hasApiList" @click="closeServer()") close Server
       v-row(no-gutters)
         v-api-list(v-model="serverStatus" :port="portNum" :isServerOn="isServerOn" style="width:100%" ) 
 </template>
